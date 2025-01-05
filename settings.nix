ea0bea38b9bce0412110a31326ff474fe3821f3c
Simon Lundius <25029432+vincentmalloy@users.noreply.github.com>
2025-01-05 11:54:21 +0100

    create default settings

---
+++ settings.nix
@@ -0,0 +1,9 @@
+{
+  username = "simon";
+  fullName = "Simon Lundius";
+  githubUser = "vincentmalloy";
+  githubId = "25029432";
+  colorscheme = "gruvbox-material-dark-medium";
+  # colorscheme = "classic-dark";
+  # colorscheme = "spacemacs";
+}
