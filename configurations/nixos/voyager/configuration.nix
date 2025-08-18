6ab9790d34b0b0c027187266e2889c8fd0350b65
Simon Lundius <25029432+vincentmalloy@users.noreply.github.com>
2025-08-18 22:32:59 +0000

    update

--- configurations/nixos/voyager/configuration.nix
+++ configurations/nixos/voyager/configuration.nix
@@ -7,11 +7,9 @@
   imports = [
     ./hardware-configuration.nix
     inputs.disko.nixosModules.default
-    (import ./disko.nix {device = "/dev/nvme0n1";})
+    (import ./disko.nix {device = "/dev/nvme1n1";})
     ./includes
   ];
-
-  internal.modules.hardware.nvidia.enable = true;
 
   nixpkgs.hostPlatform = "x86_64-linux";
   system.stateVersion = "25.05";
--- modules/home/gui/desktop-environment/hyprland/default.nix
+++ modules/home/gui/desktop-environment/hyprland/default.nix
@@ -111,9 +111,9 @@ in {
           "$mod, mouse:273, resizewindow"
         ];
         monitor = [
-          "HDMI-A-1, preferred, 0x0, 1.5"
-          "DP-1, preferred, auto-right, 1"
-          "DP-2, preferred, auto-left, 1"
+          "DP-1, preferred, 0x0, 1.75"
+          "DP-2, preferred, auto-right, 1"
+          "DP-3, preferred, auto-left, 1"
         ];
         decoration = {
           blur = {
